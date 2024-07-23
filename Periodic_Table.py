4a473dc1eb7b38619805f0777b50815bbaf099ba
ohmic-stuffs <omkarankit2004@gmail.com>
2024-07-23 21:46:12 +0530

    first push

---
+++ Periodic_Table.py
@@ -0,0 +1,9 @@
+import periodictable
+#getting details
+Atomic_No =int(input("Enter Atomic Number :"))
+element = periodictable.elements[Atomic_No]
+print(f'Atomic No : {element.number}')
+print(f"Symbol : {element.symbol}")
+print(f"Name : {element.name}")
+print(f"Atomic Mass : {element.mass}")
+print(f"Density : {element.density}")
